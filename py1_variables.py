f549a7dd5eba40bacbb9a398ee65d952e03bf284
jonhjortur <jonhjortur@jonhjortur.com>
2021-11-14 22:53:50 +0000

    Breytti helling af breytunöfnum og skipti kóðanum aðeins upp.

--- py1_variables.py
+++ py1_variables.py
@@ -1,4 +1,4 @@
-from support_magic import var_info
+from var_info import information
 # 0
 # Variables and data types
 # Breytur og gagnatög
@@ -20,146 +20,146 @@ from support_magic import var_info
 # int
 my_int = 1729
 print("my_int:", my_int)
-var_info(my_int, isl=True)
+information(my_int)
 
-#
-# # 2
-# # float
-# # Það er ekki endalaus nákvæmni á float tölu
-# # En það eru til aðrar leiðir til að fá mikið meiri nákvæmni
-# my_float = 6.283185307179586476
-# print("my_float:", my_float)
-# var_info(my_float)
-#
-#
-# # 3
-# # complex
-# my_complex_number = 2+3j
-# print("my_complex_number:", my_complex_number)
-# var_info(my_complex_number)
-# # 3.1
-# print("my_complex_number.real:", my_complex_number.real)
-# my_complex_real = my_complex_number.real
-# var_info(my_complex_real)
-# print("my_complex_number.imag:", my_complex_number.imag)
-# my_complex_imag = my_complex_number.imag
-# var_info(my_complex_imag)
-#
-#
-# # 4
-# ################
-# # boolean type #
-# ################
-#
-# my_false_bool = False
-# my_true_bool = True
-# print("my_false_bool:", my_false_bool)
-# var_info(my_true_bool)
-# print("my_true_bool:",  my_true_bool)
-# var_info(my_false_bool)
-#
-# # 5
-# ##################
-# # Sequence types #
-# ##################
-#
-# # string
-# my_string = "Þetta er strengur (string)"
-# print("my_string:", my_string)
-# var_info(my_string)
 
-# 6
-# list
+# 2
+# float
+# Það er ekki endalaus nákvæmni á float tölu
+# En það eru til aðrar leiðir til að fá mikið meiri nákvæmni
+my_float = 6.283185307179586476
+print("my_float:", my_float)
+information(my_float)
+
+
+# 3
+# complex
+my_complex_number = 2+3j
+print("my_complex_number:", my_complex_number)
+information(my_complex_number)
+# 3.1
+print("my_complex_number.real:", my_complex_number.real)
+my_complex_real = my_complex_number.real
+information(my_complex_real)
+print("my_complex_number.imag:", my_complex_number.imag)
+my_complex_imag = my_complex_number.imag
+information(my_complex_imag)
+
+
+# 4
+################
+# boolean type #
+################
+
+my_false_bool = False
+my_true_bool = True
+print("my_false_bool:", my_false_bool)
+information(my_true_bool)
+print("my_true_bool:",  my_true_bool)
+information(my_false_bool)
+
+# 5
+##################
+# Sequence types #
+##################
+
+# string
+my_string = "Þetta er strengur (string)"
+print("my_string:", my_string)
+information(my_string)
+
+6
+list
 list_item = 2021
 #   Index 0      1         2      3   4          5   6
 my_list = [6.28, "Python", False, 15, list_item, 15, "15"]
 print("my_list:", my_list)
-var_info(my_list)
+information(my_list)
 # 6.1
 print("my_list[0]:", my_list[0])
 my_list_at_0 = my_list[0]
-var_info(my_list_at_0)
+information(my_list_at_0)
 
 
-# 7
-# tuple
+7
+tuple
 my_tuple = (6.28, "Python", "-1", 15, True, 15, "15")
 print("my_tuple:", my_tuple)
-var_info(my_tuple)
+information(my_tuple)
 
-# # 8
-# #######
-# # set #
-# #######
+# 8
+#######
+# set #
+#######
 
 my_set = {6.28, "Python", False, 15, list_item, 15, "15"}
 print("my_set:", my_set)
-var_info(my_set)
-print(my_set)
-# # 9
-# ##############
-# # dictionary #
-# ##############
-#
-# my_dict = {"character": "Winston Smith", "book": "1984"}
-# print("my_dict:", my_dict)
-# var_info(my_dict)
-# # 9.1
-# print("my_dict.keys():", my_dict.keys())
-# my_dick_keys = my_dict.keys()
-# var_info(my_dick_keys)
-# print("my_dict.values():", my_dict.values())
-# my_dict_values = my_dict.values()
-# var_info(my_dict_values)
-#
-# # 10
-# ##################
-# # class / object #
-# ##################
-#
-# # Okkar eigið gagnatag
-# # Þetta er heilt concept í forritun
-# # Object oriented programming / Hlutbundin forritun
-#
-#
-# # 11
-# class Player:
-#
-#     def __init__(self, name, age, number, position, team, nationality):
-#         self.name = name
-#         self.age = age
-#         self.number = number
-#         self.position = position
-#         self.team = team
-#         self.nationality = nationality
-#
-#     def __str__(self):
-#         return f"The {self.age} years old {self.nationality} {self.position.lower()} {self.name} wears number " \
-#                f"{self.number} for {self.team}."
-#
-#
-# my_player = Player("Grant Hanley", 29, 5, "Defender", "Norwich", "Scottish")
-#
-#
-# # 12
-# print("my_player.name:", my_player.name)
-# my_player_name = my_player.name
-# var_info(my_player_name)
-# # 12.1
-# print("my_player.age:", my_player.age)
-# my_player_age = my_player.age
-# var_info(my_player_age)
-# # 12.2
-# print("my_player.number:", my_player.number)
-# print("my_player.position:", my_player.position)
-# print("my_player.team:", my_player.team)
-# print("my_player.nationality:", my_player.nationality)
-# # 12.3
-# print(my_player)
-# var_info(my_player)
-#
-# # 13
-# # Galdrar til að sjá af hvaða gagnatagi breyturnar eru.
-# varList = [my_int, my_float, my_complex_number, my_false_bool, my_string, my_list, my_set, my_tuple, my_dict, my_player]
-#
-# var_info(varList, False, True)
+information(my_set)
+
+# 9
+##############
+# dictionary #
+##############
+
+my_dictionary = {"character": "Winston Smith", "book": "1984", "rating": 10}
+print("my_dict:", my_dictionary)
+information(my_dictionary)
+# 9.1
+print("my_dict.keys():", my_dictionary.keys())
+my_dictionary_keys = my_dictionary.keys()
+information(my_dictionary_keys)
+print("my_dict.values():", my_dictionary.values())
+my_dictionary_values = my_dictionary.values()
+information(my_dictionary_values)
+
+# 10
+##################
+# class / object #
+##################
+
+# Okkar eigið gagnatag
+# Þetta er heilt concept í forritun
+# Object oriented programming / Hlutbundin forritun
+
+
+# 11
+class Player:
+
+    def __init__(self, name, age, number, position, team, nationality):
+        self.name = name
+        self.age = age
+        self.number = number
+        self.position = position
+        self.team = team
+        self.nationality = nationality
+
+    def __str__(self):
+        return f"The {self.age} years old {self.nationality} {self.position.lower()} {self.name} wears number " \
+               f"{self.number} for {self.team}."
+
+
+my_player = Player("Grant Hanley", 29, 5, "Defender", "Norwich", "Scottish")
+
+
+# 12
+print("my_player.name:", my_player.name)
+my_player_name = my_player.name
+information(my_player_name)
+# 12.1
+print("my_player.age:", my_player.age)
+my_player_age = my_player.age
+information(my_player_age)
+# 12.2
+print("my_player.number:", my_player.number)
+print("my_player.position:", my_player.position)
+print("my_player.team:", my_player.team)
+print("my_player.nationality:", my_player.nationality)
+# 12.3
+print(my_player)
+information(my_player)
+
+# 13
+# Galdrar til að sjá af hvaða gagnatagi breyturnar eru.
+varList = [my_int, my_float, my_complex_number, my_false_bool, my_string, my_list, my_set, my_tuple, my_dictionary, my_player]
+
+information(varList, False, True)
---
+++ var_info.py
@@ -0,0 +1,100 @@
+# This code is not intended to be beautiful or clever, it should work the way I want it...
+# ...which it does!!
+
+import inspect
+import sys
+
+
+def _parse_var_name(var):
+    for f in reversed(inspect.stack()):
+        return [k for k, v in f.frame.f_locals.items() if v is var][0]
+
+
+def _assemble(var, box=True, iter_list=False, iter_name="", list_idx=0, iter_set=False, iter_dict=False):
+    var_name, var_addr, var_type, var_size = \
+        _get_var_info(var, iter_list, iter_name, list_idx, iter_set, iter_dict)
+    name, value, memory_address, datatype, instance_size = \
+        _make_lines(iter_dict, var, var_name, var_addr, var_type, var_size)
+
+    _print(box, iter_dict, name, value, memory_address, datatype, instance_size)
+
+
+def _get_var_info(var, iter_list, iter_name, list_idx, iter_set, iter_dict):
+    var_name = f'{iter_name}[{list_idx}]' if iter_list \
+        else f'{iter_name} @ {list_idx}' if iter_set \
+        else f'{iter_name}' if iter_dict \
+        else _parse_var_name(var)
+
+    var_addr = hex(id(var))
+    var_type = type(var)
+    var_size = sys.getsizeof(var)
+
+    return var_name, var_addr, var_type, var_size
+
+
+def _make_lines(iter_dict, var, var_name, var_addr, var_type, var_size, value=""):
+    if iter_dict:
+        name, value = f'Key: {var_name}', f'Value: {var}'
+    else:
+        name = f'Name: {var_name}'
+    memory_address = f'Memory address: {var_addr}'
+    datatype = f'Datatype: {var_type}'
+    instance_size = f'Size in memory: {var_size} bytes'
+
+    return name, value, memory_address, datatype, instance_size
+
+
+def _print(box, iter_dict, name, value, memory_address, datatype, instance_size):
+    line_lengths = [len(name), len(value), len(memory_address), len(datatype), len(instance_size)]
+    longest_line = max(line_lengths)
+    box_width = longest_line + 4
+    title = "Information"
+    box_top_symbols = box_width - (len(title)) - 4
+
+    first_end_padding = longest_line - line_lengths[0]
+    second_end_padding = longest_line - line_lengths[1]
+    third_end_padding = longest_line - line_lengths[2]
+    forth_end_padding = longest_line - line_lengths[3]
+    fifth_end_padding = longest_line - line_lengths[4]
+
+    new_line = "\n"
+    side_symbol = "|"
+    top_symbol = "*"  # Overscore -> ‾
+    bottom_symbol = "*"
+    front_symbol = f'{side_symbol} ' * box
+    back_symbol = f' {side_symbol}' * box
+
+    print(f'{new_line * box}{front_symbol}'
+          f'{title * box} {top_symbol * box_top_symbols * box}{side_symbol * box}{new_line}', end='')
+    print(f'{front_symbol}{name}'
+          f'{" " * first_end_padding * box}{back_symbol}{new_line}', end='')
+    if iter_dict:
+        print(f'{front_symbol}{value}'
+              f'{" " * second_end_padding * box}{back_symbol}{new_line}', end='')
+    print(f'{front_symbol}{memory_address}'
+          f'{" " * third_end_padding}{back_symbol}{new_line}', end='')
+    print(f'{front_symbol}{datatype}'
+          f'{" " * forth_end_padding}{back_symbol}{new_line}', end='')
+    print(f'{front_symbol}{instance_size}'
+          f'{" " * fifth_end_padding * box}{back_symbol}{new_line}'
+          f'{bottom_symbol * box_width * box}{new_line * box}')
+
+
+def information(var, box=True, is_list_of_vars=False):
+
+    if isinstance(var, list) and is_list_of_vars:
+        for v in var:
+            _assemble(v, box)
+    elif isinstance(var, (list, tuple)):
+        _assemble(var)
+        for idx, item in enumerate(var):
+            _assemble(item, iter_list=True, iter_name=_parse_var_name(var), list_idx=idx)
+    elif isinstance(var, set):
+        for idx, item in enumerate(var):
+            _assemble(item, iter_name=_parse_var_name(var), list_idx=idx, iter_set=True)
+    elif isinstance(var, dict):
+        print(var)
+        for k, v in var.items():
+            _assemble(v, iter_name=k, iter_dict=True)
+    else:
+        _assemble(var, box)
